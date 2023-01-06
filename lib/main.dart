b9841188df92c89f5612692e673202fddb01fc59
mxgodev <mxgodev@gmail.com>
2023-01-06 15:59:54 +0200

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
+import 'package:mx_flutter_starter/src/common/data/dependencies.dart';
+import 'package:mx_flutter_starter/src/common/initialization/initialization.dart';
 import 'package:mx_flutter_starter/src/common/util/logging.dart';
+import 'package:mx_flutter_starter/src/common/widget/app.dart';
+import 'package:mx_flutter_starter/src/common/widget/error_app.dart';
 
 void main() {
   runZonedGuarded(
-    () {},
-    (error, stack) => logger.severe(error, stack),
+    () async {
+      try {
+        await $initializeApp();
+      } on Object catch (e, s) {
+        logger.severe("App initialization error", e, s);
+        runApp(const ErrorApp());
+        return;
+      }
+
+      final IDependencies dependencies = Dependencies();
+      runApp(App(dependencies: dependencies));
+    },
+    (error, stack) => logger.severe("Zone error", error, stack),
   );
 }
---
+++ lib/src/common/widget/error_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ErrorApp extends StatelessWidget {
+  const ErrorApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
